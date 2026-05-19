5a48bb02e831b7ca1e42150f9f3bb39f155436b2
javi <fruiqui413@g.educaand.es>
2026-05-19 18:22:36 +0200

    Prueba Para cambio de version (11)

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def read_root():
 
 @app.get("/status")
 def status():
-    return {"message": "FastAPI RuizQuiros-FranciscoJavier v.2.2"}
+    return {"message": "FastAPI RuizQuiros-FranciscoJavier v.2.3"}
 
 
 @app.get("/db-check")
